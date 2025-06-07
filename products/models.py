eac5cd3a787e4400c77e31658bc52bcd31d64aef
pniyitanga250 <peacemakerproofficial@gmail.com>
2025-06-07 21:27:11 +0200

    2

--- products/models.py
+++ products/models.py
@@ -1,6 +1,11 @@
 from django.db import models
 import os
 from django.conf import settings
+from storages.backends.s3boto3 import S3Boto3Storage
+
+class MediaStorage(S3Boto3Storage):
+    location = 'media'
+    file_overwrite = True
 
 class Category(models.Model):
     """Model representing product categories."""
@@ -22,7 +27,7 @@ class Product(models.Model):
     description = models.TextField()
     price = models.DecimalField(max_digits=10, decimal_places=2)
     stock_quantity = models.IntegerField()
-    image = models.ImageField(upload_to='products/')
+    image = models.ImageField(upload_to='products/', storage=MediaStorage())
     category = models.ForeignKey(
         Category,
         on_delete=models.CASCADE,
